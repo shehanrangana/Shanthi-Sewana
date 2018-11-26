43f4d04507aca7827f717de886684e8309c98692
shehanrangana <shehanrnet@gmail.com>
2018-11-26 16:32:35 +0530

    Admin page created

---
+++ Views/Admin/sidebar.php
@@ -0,0 +1,33 @@
+<!-- Sidebar -->
+<ul class="sidebar navbar-nav">
+        <li class="nav-item active">
+          <a class="nav-link" href="index.html">
+            <i class="fas fa-fw fa-tachometer-alt"></i>
+            <span>Dashboard</span>
+          </a>
+        </li>
+        
+        <li class="nav-item">
+          <a class="nav-link" href="advertisements_page.php">
+            <i class="fas fa-fw fa-chart-area"></i>
+            <span>Advertisements</span></a>
+        </li>
+
+        <li class="nav-item">
+          <a class="nav-link" href="helper_page.php">
+            <i class="fas fa-fw fa-table"></i>
+            <span>Helpers</span></a>
+        </li>
+
+        <li class="nav-item">
+          <a class="nav-link" href="tables.html">
+            <i class="fas fa-fw fa-table"></i>
+            <span>Customers</span></a>
+        </li>
+
+        <li class="nav-item">
+          <a class="nav-link" href="tables.html">
+            <i class="fas fa-fw fa-table"></i>
+            <span>Feedbacks</span></a>
+        </li>
+      </ul>
